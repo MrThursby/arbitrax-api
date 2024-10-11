4aeccbb64e181f602c036eafb7fa4b79c7be698f
MrThursby <MrThursby>
2024-10-11 11:32:24 +0300

    filters

--- app/Http/Controllers/DirectionController.php
+++ app/Http/Controllers/DirectionController.php
@@ -3,26 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Direction;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Http\Request;
 
 class DirectionController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        if ($directions = Cache::get('directions')) {
-            return $directions;
-        }
-
         $directions = Direction::query()
             ->with([
                 'bidCurrency',
                 'askCurrency',
                 'stockMarket',
-            ])
-            ->limit(50)->get();
+            ]);
 
-        Cache::set('directions', $directions);
+        $ask_currency = $request->input('ask_currency');
+        if ($ask_currency) $directions->whereHas('askCurrency', fn (Builder $query) => $query->where('name', $ask_currency));
+        
+        $bid_currency = $request->input('bid_currency');
+        if ($bid_currency) $directions->whereHas('bidCurrency', fn (Builder $query) => $query->where('name', $bid_currency));
 
-        return $directions;
+        // Cache::set('directions', $directions);
+
+        return $directions->paginate(50);
     }
 }
